3528bd4fc2aab4c83018b9abc7a870f6ba8865a1
MPARANTHAMAN <42861773+MPARANTHAMAN@users.noreply.github.com>
2019-02-18 15:02:07 +0530

    Create greater_no.py

---
+++ greater_no.py
@@ -0,0 +1,31 @@
+a = int(input("a=")) 
+b = int(input("b="))
+c = int(input("c="))
+d = int(input("d="))
+e = int(input("e="))
+f = int(input("f="))
+g = int(input("g="))
+h = int(input("h="))
+i = int(input("i="))
+j = int(input("j="))
+
+if(a>b and a>c and a>d and a>e and a>f and a>g and a>h and a>i and a>j):
+  print(a)
+elif(b>a and b>c and b>d and b>e and b>f and b>g and b>h and b>i and b>j):
+  print(b)
+elif(c>a and c>b and c>d and c>e and c>f and c>g and c>h and c>i and c>j):
+  print(c) 
+elif(d>a and d>b and d>c and d>e and d>f and d>g and d>h and d>i and d>j):
+  print(d) 
+elif(e>a and e>b and e>c and e>d and e>f and e>g and e>h and e>i and e>j):
+  print(e)
+elif(f>a and f>b and f>c and f>d and f>e and f>g and f>h and f>i and f>j):
+  print(f)
+elif(g>a and g>b and g>c and g>d and g>e and g>f and g>h and g>i and g>j):
+  print(g)
+elif(h>a and h>b and h>c and h>d and h>e and h>f and h>g and h>i and h>j):
+  print(h)
+elif(i>a and i>b and i>c and i>d and i>e and i>f and i>g and i>h and i>j):
+  print(i)
+elif(j>a and j>b and j>c and j>d and j>e and j>f and j>g and j>h and j>i):
+  print(j)
